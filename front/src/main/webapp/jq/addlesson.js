994ecb25a890a0bed6c4e242b58c8f4272638815
JongWoo <sitsumonyou@nate.com>
2022-07-13 12:22:00 +0900

    Fix addlesson.html & addlesson.js

--- front/src/main/webapp/jq/addlesson.js
+++ front/src/main/webapp/jq/addlesson.js
@@ -1,28 +1,26 @@
 $(function(){
-   
     //------------썸네일 첨부 START------------
 
-    //------------레슨분류 등록 START------------
-
-    //------------레슨지역 등록 START------------
-
+    //------------지역 START------------
+    //------------레슨분류 START------------
+    
+	 
     //------------레슨정보 등록버튼 START------------
-	$('div.tr3>div.tr1>button').click(function(){
-		$.ajax({
-			url: '/back/addorder',
-            method: 'post',
-			success: function(jsonObj){
-				if(jsonObj.status == 1){	//로그인이 되있을경우
-					alert(jsonObj.msg);
-					location.href="";
-				}else if(jsonObj.status == -1){	//등록실패
-					alert(jsonObj.msg);
-                    location.href="";
-				}
-			},
-			error: function(jqXHR){
-				alert('오류: ' + jqXHR.status);
-			}
-		});
-	});
+    let $form = $('form');
+    $form.submit(function(){
+        let data = $(this).serialize();
+        console.log(data);
+        $.ajax({
+            url : "http://localhost:1124/back/addlesson",
+            method : 'post',
+            data : data,
+            success : function(jsonObj){
+                alert(jsonObj.msg);
+            },
+            error : function(jqXHR){
+                alert('오류 : ' + jqXHR.status);
+            }
+        });
+        return false;
+    });
 });
